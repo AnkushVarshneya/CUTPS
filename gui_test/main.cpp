654838a8e1096778a7271e1d5ded423c9d1010f9
admin <admin@COMP3004.(none)>
2014-10-16 15:31:26 -0400

    Merge branch 'ryan'
    Conflicts:
    	gui_test/main.cpp

--- gui_test/main.cpp
+++ gui_test/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    "Hello this is a test lolol"
+    //"Hello this is a test lolol"
     return a.exec(); // its changed!!!!!!!!!!
 
     // more coments!!!!!!!!234 ankush was here

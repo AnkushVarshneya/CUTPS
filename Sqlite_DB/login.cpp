15c861abb22ee3dc07e2efef59c58381d32a0b12
ankush <a@a.a>
2014-10-17 16:31:32 -0400

    dsf

--- Sqlite_DB/login.cpp
+++ Sqlite_DB/login.cpp
@@ -7,13 +7,18 @@ Login::Login(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QSqlDatabase mydb=QSqlDatabase::addDatabase("QSQLITE");
-    mydb.setDatabaseName("../gui_test/testDB");
+    QSqlDatabase db=QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("../gui_test/testDB");
 
-    if(!mydb.open())
+    if(!db.open())
         ui->label->setText("Failed to open the database");
-    else
+    else {
         ui->label->setText("connected....");
+        QSqlQuery query;
+        query.exec("INSERT INTO test(x,y,z) VALUES(10101, 'test', 'test2')");
+        //query.exec("SELECT * FROM test");
+        db.commit();
+    }
 }
 
 Login::~Login()

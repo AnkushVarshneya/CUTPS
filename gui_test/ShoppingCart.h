853e75e433bb9390c351b665b8424cfcda1e05ed
admin <admin@COMP3004.(none)>
2014-10-24 14:07:52 -0400

    Added skeletons for purchasableitems, course, cart

---
+++ gui_test/ShoppingCart.h
@@ -0,0 +1,13 @@
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
+
+#include <string>
+
+class ShoppingCart{
+public:
+private:
+    Student stu;
+    QList<PurchasableItem> items;
+};
+
+#endif // SHOPPINGCART_H

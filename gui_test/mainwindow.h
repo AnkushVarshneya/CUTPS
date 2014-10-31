8b77be1271bb7444e97eac461fe471068bb394b1
admin <admin@COMP3004.(none)>
2014-10-31 17:04:46 -0400

    adding fxns to handle menubar presses

--- gui_test/mainwindow.h
+++ gui_test/mainwindow.h
@@ -31,6 +31,18 @@ private:
 private slots:
     //void displayError(QAbstractSocket::SocketError socketError);
     void readyRead();
+    void on_actionShopping_Cart_has_Items_triggered();
+    void on_actionShopping_Cart_is_Empty_triggered();
+    void on_actionViewShoppingCart_hasitems_triggered();
+    void on_actionViewShoppingCart_noitems_triggered();
+    void on_actionStudent_unenrolled_triggered();
+    void on_actionStudent_ViewTextbooks_notextbooks_triggered();
+    void on_actionStudent_ViewTextbooks_unenrolled_triggered();
+    void on_actionStudent_ViewTextbooks_hastextbooks_triggered();
+    void on_actionEmptyShoppingCart_hasitems_triggered();
+    void on_actionEmptyShoppingCart_noitems_triggered();
+    void on_actionStudent_EmptyShoppingCart_hasitems_triggered();
+    void on_actionStudent_EmptyShoppingCart_noitems_triggered();
 };
 
 #endif // MAINWINDOW_H

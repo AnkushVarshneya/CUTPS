1dee8f8a397d7f219fd1116438c775025592d965
admin <admin@COMP3004.(none)>
2014-11-05 00:36:14 -0500

    server calls?????

--- cutps_server/CUtpsServer.cpp
+++ cutps_server/CUtpsServer.cpp
@@ -114,12 +114,35 @@ void CutpsServer::readBytes() {
 
        this->sendJson(result);
    }
-//   else if (cmd == "createCourse()") {
-//       qDebug() << "processing command to create course..." << "\n";
-//       APIControl *apic = new APIControl();
-//       apic->createCourse(jsonDoc.object());
-//   }
+   else if (cmd == "createCourse()") {
+       qDebug() << "processing command to create course..." << "\n";
+       APIControl *apic = new APIControl();
+       QJsonObject result = apic->createCourse(jsonDoc.object());
+       delete apic;
+       this->sendJson(result);
+   }
+   else if (cmd == "createTextbook()") {
+       qDebug() << "processing command to create textbook..." << "\n";
+       APIControl *apic = new APIControl();
+       QJsonObject result = apic->createTextbook(jsonDoc.object());
+       delete apic;
+       this->sendJson(result);
+   }
+   else if (cmd == "cManagerViewCourses()") {
+       qDebug() << "processing command to create textbook..." << "\n";
+       APIControl *apic = new APIControl();
+       QJsonObject result = apic->cManagerViewCourses(jsonDoc.object());
+       delete apic;
+       this->sendJson(result);
 
+   }
+   else if (cmd == "cManagerViewTextbooks()") {
+       qDebug() << "processing command to create textbook..." << "\n";
+       APIControl *apic = new APIControl();
+       QJsonObject result = apic->cManagerViewTextbooks(jsonDoc.object());
+       delete apic;
+       this->sendJson(result);
+   }
 } //readbytes
 
 void CutpsServer::sendJson(QJsonObject &json) {

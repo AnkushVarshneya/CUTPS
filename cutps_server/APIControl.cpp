e02a74e6eecb31c5841f32a8574c1664896deb7c
admin <admin@COMP3004.(none)>
2014-11-04 21:15:02 -0500

    message

--- cutps_server/APIControl.cpp
+++ cutps_server/APIControl.cpp
@@ -28,3 +28,56 @@ QJsonObject APIControl::studentViewTextbooks(QJsonObject json) {
     qDebug() << r;
     return r;
 }
+QJsonObject APIControl::getExistingPaymentInfo(QJsonObject json) {
+    QString stuNum = json["Student Number"].toString();
+
+    QString testNum = "100853074";
+
+    QueryControl *query = new QueryControl();
+    PaymentInformation p;
+    p = query->getExistingBillingInfo(testNum);  //100853074
+    delete query;
+    QJsonObject payment;
+    QJsonDocument result;
+    p.write(payment);
+    result = QJsonDocument(payment);
+    qDebug() << result.toJson();
+
+}
+
+QJsonObject APIControl::createTextbook(QJsonObject json) {
+
+}
+
+
+QJsonObject APIControl::viewShoppingCart(QJsonObject json, QMap<QString, ShoppingCart> testStudentShoppingCart) {
+    QJsonObject result;
+    QString stuNum = json["Student Number"].toString();
+    ShoppingCart *cart;
+       if(testStudentShoppingCart.contains(stuNum)) {
+       *cart = testStudentShoppingCart.value(stuNum);
+       }
+    cart->write(result);
+    return result;
+}
+
+//QJsonObject APIControl::cManagerViewTextbooks(QJsonObject json) {
+
+//    Term term;
+//    term.read(json["Term"].toObject());
+//    QueryControl *query = new QueryControl();
+//    //
+//    delete query;
+//    QJsonArray resultArray;
+//    foreach(Course *crs, result){
+//        QJsonObject json;
+//        crs->write(json);
+//        resultArray.append(json);
+//    }
+
+//    QJsonObject r;
+//    r["courses:"] = resultArray;
+//    qDebug() << r;
+//    return r;
+
+//}

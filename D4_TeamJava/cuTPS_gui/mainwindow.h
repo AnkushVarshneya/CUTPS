95c52534360cd4c9cdcbeef444dfe37404ec56a2
admin <admin@COMP3004.(none)>
2014-11-27 15:12:29 -0500

    Added pictures to UI for ContentManager

--- D4_TeamJava/cuTPS_gui/mainwindow.h
+++ D4_TeamJava/cuTPS_gui/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "ContentManagerInterfaceWindow.h"
 
 namespace Ui {
 class MainWindow;
@@ -22,6 +23,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    ContentManagerInterfaceWindow contentManagerUI;
 
 
 };

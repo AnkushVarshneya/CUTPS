5b95bcc070ea7efdbfac3a2185733c2baaee50d0
admin <admin@COMP3004.(none)>
2014-11-28 15:31:17 -0500

    compiles lol

---
+++ D4_TeamJava/cuTPS_gui/StudentInterfaceWindow.h
@@ -0,0 +1,22 @@
+#ifndef STUDENTINTERFACEWINDOW_H
+#define STUDENTINTERFACEWINDOW_H
+
+#include <QWidget>
+
+namespace Ui {
+class StudentInterfaceWindow;
+}
+
+class StudentInterfaceWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit StudentInterfaceWindow(QWidget *parent = 0);
+    ~StudentInterfaceWindow();
+
+private:
+    Ui::StudentInterfaceWindow *ui;
+};
+
+#endif // STUDENTINTERFACEWINDOW_H

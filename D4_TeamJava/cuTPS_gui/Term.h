97bb52c6a0df3316204030d001550561965cc8a5
admin <admin@COMP3004.(none)>
2014-11-28 13:19:24 -0500

    Added CUtpsDataObject files from gui_test, Modified Term class files to not contain courses anymore as we don't need that in our system (only need relationship in the database), added types.h header for easily including all entity objects when declaring in other classes (such as control classes)

---
+++ D4_TeamJava/cuTPS_gui/Term.h
@@ -0,0 +1,45 @@
+/*Term header class
+ *
+ *Purpose of this class: An academic term, usually 4 months in length (Summer terms can be considered
+ *to have 2 month sub-terms). A term will have a start and end date.
+ *Will be used by Students to easily search for Textbooks for the registered courses they are in for a given term
+ *
+ *Traceability: EO-09
+ */
+#ifndef TERM_H
+#define TERM_H
+
+#include <QString>
+#include <QDate>
+
+#include "CUtpsDataObject.h"
+
+using namespace std;
+
+
+
+
+
+class Term : CUtpsDataObject{
+public:
+    Term(QDate = QDate::fromString("20140905","yyyyMMdd"),QDate = QDate::fromString("20141209","yyyyMMdd"), QString = "fall2014");
+    ~Term();
+
+    QDate           getStartDate() const;
+    QDate           getEndDate() const;
+    QString         getTermID() const;
+    void            setStartDate(int,int,int);
+    void            setEndDate(int,int,int);
+    void            setTermID(QString);
+    void            read(const QJsonObject &json);
+    void            write(QJsonObject &json) const;
+
+private:
+    QDate           startDate;
+    QDate           endDate;
+    QString         termID;
+};
+
+
+
+#endif // TERM_H

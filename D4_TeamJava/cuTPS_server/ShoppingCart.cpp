22aaa121a2aba658409fc89eeadfae216a57ba26
admin <admin@COMP3004.(none)>
2014-12-05 13:47:53 -0500

    Changed read and write implementations of ShoppingCart to accomodate a QList of QPairs In the QPairs we have <PurchsableItem*,qint32> where qint32 is the quantity

--- D4_TeamJava/cuTPS_server/ShoppingCart.cpp
+++ D4_TeamJava/cuTPS_server/ShoppingCart.cpp
@@ -29,42 +29,46 @@ void ShoppingCart::emptyShoppingCart(){
 //Read and write json concrete functions
 
 void ShoppingCart::read(const QJsonObject &json){
-    /*
+
     QJsonArray itemArray= json["items"].toArray();
-    for(int i = 0; i < itemArray.size(); ++i){
-        QJsonObject itemObject = itemArray[i].toObject();
+    for(int i = 0; i<itemArray.size(); ++i){
+        QJsonObject pairObject = itemArray[i].toObject();
+        QJsonObject itemObject = pairObject["item"].toObject();
+        QPair<PurchasableItem*,qint32> newPair;
         if(itemObject.contains("isbn")){
             Textbook* newTextbook = new Textbook();
             newTextbook->read(itemObject);
-            items.append(newTextbook);
+            newPair.first = newTextbook;
+            newPair.second = pairObject["quantity"].toDouble();
+            items.append(newPair);
+
         }
-        else if(itemObject.contains("chapterNumber")){
+        else if (itemObject.contains("chapterNumber")){
             Chapter* newChapter = new Chapter();
             newChapter->read(itemObject);
-            items.append(newChapter);
+            newPair.first = newChapter;
+            newPair.second = pairObject["quantity"].toDouble();
+            items.append(newPair);
         }
         else if(itemObject.contains("sectionNumber")){
             Section* newSection = new Section();
             newSection->read(itemObject);
-            items.append(newSection);
+            newPair.first = newSection;
+            newPair.second = pairObject["quantity"].toDouble();
+            items.append(newPair);
         }
     }
-    */
 }
 
 void ShoppingCart::write(QJsonObject &json) const{
-
-    /*
     QJsonArray itemArray;
-    foreach(QPair<PurchasableItem*,qint32> item, items){
+    for (int i = 0; i < items.size(); i++){
         QJsonObject pairObject;
         QJsonObject itemObject;
-
-        itemObject["item"]
-        item->write(itemObject);
-        itemArray.append(itemObject);
+        items[i].first->write(itemObject);
+        pairObject["item"] = itemObject;
+        pairObject["quantity"] = items[i].second;
+        itemArray.append(pairObject);
     }
     json["items"] = itemArray;
-    */
-
 }
